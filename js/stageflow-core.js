f39304c437900f3ed2b4f4f2acf2caad4e673bfe
Erick Lopes <ercklopes@gmail.com>
2026-04-08 10:46:42 -0300

    Delete js directory

--- js/stageflow-core.js
+++
@@ -1,8 +0,0 @@
-﻿(function () {
-  "use strict";
-  function uid() {
-    if (typeof crypto !== "undefined" && crypto.randomUUID) return crypto.randomUUID();
-    return "id-" + Date.now() + "-" + Math.random().toString(36).slice(2, 11);
-  }
-  window.stageflowCore = { uid: uid };
-})();
